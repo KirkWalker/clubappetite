0138e1e27ab759c34a279418d587f39c3462d4b6
Kirk Walker <kirkwalker@Kirks-MacBook-Pro.local>
2016-03-08 05:56:42 -0800

    New class for information pages

---
+++ datalayer/InfoPage.js
@@ -0,0 +1,27 @@
+var { View } = require('react-native')
+
+var DB = require('./DB');
+
+
+module.exports = {
+
+    getFaqPageData(_this) {
+        DB.infopage.get_all(function(result){
+
+
+            console.log(result);
+            //_this.setState({count: result.totalrows, dataObj: result});
+        })
+    },
+
+    getTermPageData(_this) {
+        DB.infopage.get_all(function(result){
+
+
+
+            console.log(result);
+            //_this.setState({count: result.totalrows, dataObj: result});
+        })
+    }
+
+}
