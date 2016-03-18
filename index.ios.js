80fbe8a794f4d5e5b48e668f92407a0506d7043c
Kirk Walker <kirkwalker@Kirks-MacBook-Pro.local>
2016-03-17 22:31:22 -0700

    enabled splash page again

--- index.ios.js
+++ index.ios.js
@@ -229,7 +229,7 @@ class AMGSandbox extends Component {
         side={this.settings.rightSide ? 'right' : 'left'}
       >
         <Navigator
-          initialRoute={{id: 'Donate', name: 'Splash Page'}}
+          initialRoute={{id: 'SplashPage', name: 'Splash Page'}}
           renderScene={this.renderScene.bind(this)}
           configureScene={(route) => {
             if (route.sceneConfig) {
